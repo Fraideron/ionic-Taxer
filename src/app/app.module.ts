12028f1325b1ad2e569d1392e92b0e4009711001
Fraideron <jiafen24@gmail.com>
2017-04-03 12:18:48 +0300

    ionic front without tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { TabsPage } from '../pages/tabs/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
